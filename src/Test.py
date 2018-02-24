49d6301574724e71ff1176d8870fa86e5ceccab0
Carson Scott <carsonjscott14@gmail.com>
2018-02-24 18:30:26 -0500

    removed test file

--- src/Test.py
+++ src/Test.py
@@ -1,5 +1,5 @@
 from Frames import *
 
-P = Proposition(AND([IN([0, 1, 2, 3, 4]), NOT(IN([0, 1, 2]))]))
-Y = P(2)
-print(Y)
+prop = Proposition(AND([IN([0, 1, 2, 3, 4]), NOT(IN([0, 1, 2]))]))
+output = prop(2)
+print(output)
